c9614ea1891eb3a2976bb6fe82c344279a63224c
Dominic Neo <dom.neo.ws@gmail.com>
2023-08-17 20:05:05 +0800

    update styling and header layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,20 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          "DM Sans",
+          ...defaultTheme.fontFamily.sans,
+        ]
+      }
+    },
+    fontFamily: {
+      "display": ["Cormorant"]
+    }
   },
   plugins: [require("daisyui")],
   daisyui: {
@@ -11,8 +23,8 @@ module.exports = {
         crystalight: {
           primary: "#C83631",
           secondary: "#641B19",
-          accent: "#1CC7B8",
-          neutral: "#926863",
+          accent: "#C8EFE9",
+          neutral: "#876B6B",
           "base-100": "#f2f2f2",
           info: "#42aebd",
           success: "#489380",
